ef8cc37cf3523462c3cd9a864d3e7016f09b0f28
Никита <nikita-vip1904@yandex.ru>
2023-09-05 22:25:27 +0300

    сделал задание 1.2.4

---
+++ Zadanie_1_2_4.java
@@ -0,0 +1,11 @@
+public class Zadanie_1_2_4 {
+
+    public static void main(String[] args) {
+      int varint = 100;
+      long varlong = 1000;
+      double vardouble = 100.00;
+      float varfloat = 100.0000f;
+      char varchar = 122;
+      boolean varbooleaan = true;
+    }
+}
